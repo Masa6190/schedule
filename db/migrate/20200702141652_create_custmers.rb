55c160d99fd4d7865a8586cc491ca367fd7b6b7f
Masa6190 <masamitsu.nakayama1@gmail.com>
2020-07-09 23:24:01 +0900

    Update 20200702141652_create_custmers.rb

--- db/migrate/20200702141652_create_custmers.rb
+++ db/migrate/20200702141652_create_custmers.rb
@@ -1,7 +1,27 @@
 class CreateCustmers < ActiveRecord::Migration[5.2]
   def change
     create_table :custmers do |t|
-
+      t.string :familyname
+      t.string :firstname
+      t.string :familyname_kana
+      t.string :firstname_kana
+      t.integer :birth_year
+      t.integer :birth_month
+      t.integer :birth_day
+      t.string :adress
+      t.integer :postal_code
+      t.string :prefecture
+      t.string :city
+      t.string :block
+      t.string :building
+      t.integer :phone_number
+      t.string :kaigodo
+      t.integer :ninnteibi
+      t.integer :ninnteikikanstart_year
+      t.integer :ninnteikikanstart_month
+      t.integer :ninnteikikanend_year
+      t.integer :ninnteikikanend_month
+      t.string :futannwariai
       t.timestamps
     end
   end
