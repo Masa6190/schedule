23f0a7f95f322e408e1b33964314e4a0b28d2f43
Masa6190 <masamitsu.nakayama1@gmail.com>
2020-07-11 23:13:40 +0900

    Update 20200702141715_create_helpers.rb

--- db/migrate/20200702141715_create_helpers.rb
+++ db/migrate/20200702141715_create_helpers.rb
@@ -1,7 +1,21 @@
 class CreateHelpers < ActiveRecord::Migration[5.2]
   def change
     create_table :helpers do |t|
-
+      t.string :familyname
+      t.string :firstname
+      t.string :familyname_kana
+      t.string :firstname_kana
+      t.integer :birth_year
+      t.integer :birth_month
+      t.integer :birth_day
+      t.string :adress
+      t.integer :postal_code
+      t.string :prefecture
+      t.string :city
+      t.string :block
+      t.string :building
+      t.integer :phone_number
+      t.string :idousyudann
       t.timestamps
     end
   end
